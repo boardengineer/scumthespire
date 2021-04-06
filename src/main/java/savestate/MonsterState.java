0e028ee3a8c5593101a9c36d3285fe784dc2ff02
boardengineer <boardengineer88@gmail.com>
2021-04-05 22:25:18 -0700

    Encoder for MonsterState
    Signed-off-by: boardengineer <boardengineer88@gmail.com>

--- src/main/java/savestate/MonsterState.java
+++ src/main/java/savestate/MonsterState.java
@@ -1,34 +1,42 @@
 package savestate;
 
 import basemod.ReflectionHacks;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
 import com.megacrit.cardcrawl.monsters.exordium.*;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MonsterState extends CreatureState {
+    private static final String DAMAGE_DELIMETER = ";";
+    private static final String MOVE_HISTORY_DELIMETER = "&";
+
     private final float deathTimer;
     private final boolean tintFadeOutCalled;
     private final boolean escaped;
     private final boolean escapeNext;
-    private final AbstractMonster.EnemyType type;
     private final boolean cannotEscape;
-    private final ArrayList<DamageInfoState> damage;
-    private final ArrayList<Byte> moveHistory;
     private final byte nextMove;
-    private final Hitbox intentHb;
-    private final AbstractMonster.Intent intent;
-    private final AbstractMonster.Intent tipIntent;
+    private final HitboxState intentHb;
     private final float intentAlpha;
     private final float intentAlphaTarget;
     private final float intentOffsetX;
     private final String moveName;
     private final EnemyMoveInfoState moveInfo;
+
+    private final AbstractMonster.EnemyType type;
+
+    private final AbstractMonster.Intent intent;
+    private final AbstractMonster.Intent tipIntent;
+
+    private final ArrayList<Byte> moveHistory;
+    private final ArrayList<DamageInfoState> damage;
 
     public MonsterState(AbstractMonster monster) {
         super(monster);
@@ -41,7 +49,7 @@ public class MonsterState extends CreatureState {
         this.cannotEscape = monster.cannotEscape;
 
         this.nextMove = monster.nextMove;
-        this.intentHb = monster.intentHb;
+        this.intentHb = new HitboxState(monster.intentHb);
         this.intent = monster.intent;
         this.tipIntent = monster.tipIntent;
         this.intentAlpha = monster.intentAlpha;
@@ -59,6 +67,45 @@ public class MonsterState extends CreatureState {
 
         this.moveHistory = monster.moveHistory.stream().map(Byte::byteValue)
                                               .collect(Collectors.toCollection(ArrayList::new));
+
+    }
+
+    public MonsterState(String jsonString) {
+        super(new JsonParser().parse(jsonString).getAsJsonObject().get("creature").getAsString());
+
+        JsonObject parsed = new JsonParser().parse(jsonString).getAsJsonObject();
+
+        this.deathTimer = parsed.get("death_timer").getAsFloat();
+        this.tintFadeOutCalled = parsed.get("tint_fade_out_called").getAsBoolean();
+        this.escaped = parsed.get("escaped").getAsBoolean();
+        this.escapeNext = parsed.get("escape_next").getAsBoolean();
+        this.type = AbstractMonster.EnemyType.valueOf(parsed.get("type_name").getAsString());
+        this.cannotEscape = parsed.get("cannot_escape").getAsBoolean();
+        this.nextMove = parsed.get("next_move").getAsByte();
+        this.intentHb = new HitboxState(parsed.get("intent_hb").getAsString());
+        this.intent = AbstractMonster.Intent.valueOf(parsed.get("intent_name").getAsString());
+        this.tipIntent = AbstractMonster.Intent
+                .valueOf(parsed.get("tip_intent_name").getAsString());
+        this.intentAlpha = parsed.get("intent_alpha").getAsFloat();
+        this.intentAlphaTarget = parsed.get("intent_alpha_target").getAsFloat();
+        this.intentOffsetX = parsed.get("intent_offset_x").getAsFloat();
+        this.moveName = parsed.get("move_name").isJsonNull() ? null : parsed.get("move_name")
+                                                                            .getAsString();
+
+        this.moveInfo = new EnemyMoveInfoState(parsed.get("move_info").getAsString());
+
+        this.damage = Stream.of(parsed.get("damage").getAsString().split(DAMAGE_DELIMETER))
+                            .filter(s -> !s.isEmpty()).map(s -> {
+                    DamageInfoState result = new DamageInfoState(s);
+                    return result;
+                })
+                            .collect(Collectors.toCollection(ArrayList::new));
+
+        this.moveHistory = Stream
+                .of(parsed.get("move_history").getAsString().split(MOVE_HISTORY_DELIMETER))
+                .filter(s -> !s.isEmpty()).map(Byte::parseByte)
+                .collect(Collectors.toCollection(ArrayList::new));
+
     }
 
     public AbstractMonster loadMonster() {
@@ -74,6 +121,7 @@ public class MonsterState extends CreatureState {
         monster.type = this.type;
         monster.cannotEscape = this.cannotEscape;
         monster.damage = this.damage.stream().map(DamageInfoState::loadDamageInfo)
+                                    .peek(state -> state.owner = monster)
                                     .collect(Collectors.toCollection(ArrayList::new));
 
         monster.setMove(moveName, moveInfo.nextMove, moveInfo.intent, moveInfo.baseDamage, moveInfo.multiplier, moveInfo.isMultiDamage);
@@ -82,7 +130,7 @@ public class MonsterState extends CreatureState {
                                               .collect(Collectors.toCollection(ArrayList::new));
 
         monster.nextMove = this.nextMove;
-        monster.intentHb = this.intentHb;
+        monster.intentHb = this.intentHb.loadHitbox();
         monster.intent = this.intent;
         monster.tipIntent = this.tipIntent;
         monster.intentAlpha = this.intentAlpha;
@@ -168,5 +216,37 @@ public class MonsterState extends CreatureState {
         }
 
         return monster;
+    }
+
+    public String encode() {
+        JsonObject monsterStateJson = new JsonObject();
+
+        monsterStateJson.addProperty("creature", super.encode());
+        monsterStateJson.addProperty("death_timer", deathTimer);
+        monsterStateJson.addProperty("tint_fade_out_called", tintFadeOutCalled);
+        monsterStateJson.addProperty("escaped", escaped);
+        monsterStateJson.addProperty("escape_next", escapeNext);
+        monsterStateJson.addProperty("cannot_escape", cannotEscape);
+        monsterStateJson.addProperty("next_move", nextMove);
+        monsterStateJson.addProperty("intent_hb", intentHb.encode());
+        monsterStateJson.addProperty("intent_alpha", intentAlpha);
+        monsterStateJson.addProperty("intent_alpha_target", intentAlphaTarget);
+        monsterStateJson.addProperty("intent_offset_x", intentOffsetX);
+        monsterStateJson.addProperty("move_name", moveName);
+        monsterStateJson.addProperty("move_info", moveInfo.encode());
+        monsterStateJson.addProperty("intent_name", intent.name());
+        monsterStateJson.addProperty("type_name", type.name());
+        monsterStateJson.addProperty("tip_intent_name", tipIntent.name());
+
+        monsterStateJson
+                .addProperty("move_history", moveHistory.stream().map(b -> String.valueOf(b))
+                                                        .collect(Collectors
+                                                                .joining(MOVE_HISTORY_DELIMETER)));
+
+        monsterStateJson.addProperty("damage", damage.stream().map(DamageInfoState::encode)
+                                                     .collect(Collectors
+                                                             .joining(DAMAGE_DELIMETER)));
+
+        return monsterStateJson.toString();
     }
 }
