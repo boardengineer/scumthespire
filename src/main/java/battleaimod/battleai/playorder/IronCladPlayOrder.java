c6eb651bd8e9a6a87465bb855e80f675d99c5ffc
boardengineer <boardengineer88@gmail.com>
2021-11-06 18:44:37 -0700

    Refactor play orders

--- src/main/java/battleaimod/battleai/playorder/IronCladPlayOrder.java
+++ src/main/java/battleaimod/battleai/playorder/IronCladPlayOrder.java
@@ -1,6 +1,5 @@
 package battleaimod.battleai.playorder;
 
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.blue.WhiteNoise;
 import com.megacrit.cardcrawl.cards.colorless.*;
 import com.megacrit.cardcrawl.cards.green.PhantasmalKiller;
@@ -9,206 +8,194 @@ import com.megacrit.cardcrawl.cards.red.*;
 
 import java.util.HashMap;
 
-public class    IronCladPlayOrder {
-    public static HashMap<String, Integer> uglyThing;
-    public static final HashMap<String, Integer> CARD_RANKS = makeRank();
+public class IronCladPlayOrder {
+    public static final HashMap<String, Integer> CARD_RANKS = new HashMap<String, Integer>() {{
+        int size = 0;
 
-    public static HashMap<String, Integer> makeRank() {
-        uglyThing = new HashMap<>();
+        put(new Omniscience().cardID, size++);
+        put(new Headbutt().cardID, size++);
+        put(new SadisticNature().cardID, size++);
+        put(new MasterReality().cardID, size++);
+        put(new Worship().cardID, size++);
+        put(new Panacea().cardID, size++);
+        put(new Madness().cardID, size++);
+        put(new Shockwave().cardID, size++);
+        put(new SpotWeakness().cardID, size++);
 
-        add(new Omniscience());
-        add(new Headbutt());
-        add(new SadisticNature());
-        add(new MasterReality());
-        add(new Worship());
-        add(new Panacea());
-        add(new Madness());
-        add(new Shockwave());
-        add(new SpotWeakness());
+        put(new Transmutation().cardID, size++);
+        put(new Rage().cardID, size++);
+        put(new Offering().cardID, size++);
+        put(new Intimidate().cardID, size++);
+        put(new Flex().cardID, size++);
+        put(new DoubleTap().cardID, size++);
+        put(new DualWield().cardID, size++);
+        put(new Clash().cardID, size++);
 
-        add(new Transmutation());
-        add(new Rage());
-        add(new Offering());
-        add(new Intimidate());
-        add(new Flex());
-        add(new DoubleTap());
-        add(new DualWield());
-        add(new Clash());
+        put(new Bloodletting().cardID, size++);
+        put(new Berserk().cardID, size++);
+        put(new BattleTrance().cardID, size++);
+        put(new Whirlwind().cardID, size++);
 
-        add(new Bloodletting());
-        add(new Berserk());
-        add(new BattleTrance());
-        add(new Whirlwind());
-
-        add(new Armaments());
-        add(new Feed());
-        add(new Enlightenment());
-        add(new HandOfGreed());
+        put(new Armaments().cardID, size++);
+        put(new Feed().cardID, size++);
+        put(new Enlightenment().cardID, size++);
+        put(new HandOfGreed().cardID, size++);
 
         // Powers first
-        add(new Inflame());
-        add(new Rupture());
-        add(new Juggernaut());
-        add(new FireBreathing());
-        add(new FeelNoPain());
-        add(new Evolve());
-        add(new DemonForm());
-        add(new Barricade());
-        add(new Brutality());
-        add(new Combust());
-        add(new Corruption());
-        add(new DarkEmbrace());
-        add(new LimitBreak());
-        add(new Metallicize());
-        add(new PhantasmalKiller());
+        put(new Inflame().cardID, size++);
+        put(new Rupture().cardID, size++);
+        put(new Juggernaut().cardID, size++);
+        put(new FireBreathing().cardID, size++);
+        put(new FeelNoPain().cardID, size++);
+        put(new Evolve().cardID, size++);
+        put(new DemonForm().cardID, size++);
+        put(new Barricade().cardID, size++);
+        put(new Brutality().cardID, size++);
+        put(new Combust().cardID, size++);
+        put(new Corruption().cardID, size++);
+        put(new DarkEmbrace().cardID, size++);
+        put(new LimitBreak().cardID, size++);
+        put(new Metallicize().cardID, size++);
+        put(new PhantasmalKiller().cardID, size++);
 
         // damage
-        add(new ThunderClap());
-        add(new Bludgeon());
-        add(new Bash());
-        add(new Uppercut());
-        add(new SearingBlow());
-        add(new Reaper());
-        add(new Rampage());
-        add(new TwinStrike());
-        add(new PommelStrike());
-        add(new HeavyBlade());
-        add(new WildStrike());
-        add(new PerfectedStrike());
-        add(new Carnage());
-        add(new InfernalBlade());
-        add(new Immolate());
-        add(new IronWave());
-        add(new FiendFire());
-        add(new Dropkick());
-        add(new Hemokinesis());
-        add(new Clothesline());
-        add(new Cleave());
-        add(new BloodForBlood());
-        add(new Pummel());
-        add(new RecklessCharge());
-        add(new SeverSoul());
-        add(new SwordBoomerang());
+        put(new ThunderClap().cardID, size++);
+        put(new Bludgeon().cardID, size++);
+        put(new Bash().cardID, size++);
+        put(new Uppercut().cardID, size++);
+        put(new SearingBlow().cardID, size++);
+        put(new Reaper().cardID, size++);
+        put(new Rampage().cardID, size++);
+        put(new TwinStrike().cardID, size++);
+        put(new PommelStrike().cardID, size++);
+        put(new HeavyBlade().cardID, size++);
+        put(new WildStrike().cardID, size++);
+        put(new PerfectedStrike().cardID, size++);
+        put(new Carnage().cardID, size++);
+        put(new InfernalBlade().cardID, size++);
+        put(new Immolate().cardID, size++);
+        put(new IronWave().cardID, size++);
+        put(new FiendFire().cardID, size++);
+        put(new Dropkick().cardID, size++);
+        put(new Hemokinesis().cardID, size++);
+        put(new Clothesline().cardID, size++);
+        put(new Cleave().cardID, size++);
+        put(new BloodForBlood().cardID, size++);
+        put(new Pummel().cardID, size++);
+        put(new RecklessCharge().cardID, size++);
+        put(new SeverSoul().cardID, size++);
+        put(new SwordBoomerang().cardID, size++);
 
-        add(new Anger());
+        put(new Anger().cardID, size++);
 
         // Block last
-        add(new SeeingRed());
-        add(new ShrugItOff());
-        add(new GhostlyArmor());
-        add(new FlameBarrier());
-        add(new Disarm());
-        add(new PowerThrough());
-        add(new Impervious());
-        add(new BurningPact());
-        add(new SecondWind());
-        add(new TrueGrit());
+        put(new SeeingRed().cardID, size++);
+        put(new ShrugItOff().cardID, size++);
+        put(new GhostlyArmor().cardID, size++);
+        put(new FlameBarrier().cardID, size++);
+        put(new Disarm().cardID, size++);
+        put(new PowerThrough().cardID, size++);
+        put(new Impervious().cardID, size++);
+        put(new BurningPact().cardID, size++);
+        put(new SecondWind().cardID, size++);
+        put(new TrueGrit().cardID, size++);
 
-        add(new Entrench());
-        add(new BodySlam());
+        put(new Entrench().cardID, size++);
+        put(new BodySlam().cardID, size++);
 
-        add(new Exhume());
+        put(new Exhume().cardID, size++);
 
-        add(new Havoc());
-        add(new Warcry());
+        put(new Havoc().cardID, size++);
+        put(new Warcry().cardID, size++);
 
-        add(new Strike_Red());
-        add(new Defend_Red());
-        add(new Sentinel());
+        put(new Strike_Red().cardID, size++);
+        put(new Defend_Red().cardID, size++);
+        put(new Sentinel().cardID, size++);
 
         // These are the watcher cards, the combined play order will be here
-        add(new WreathOfFlame());
+        put(new WreathOfFlame().cardID, size++);
 
-        add(new Ragnarok());
-        add(new TalkToTheHand());
-        add(new FlurryOfBlows());
-        add(new Halt());
-        add(new Prostrate());
-        add(new SpiritShield());
-        add(new SignatureMove());
-
-
+        put(new Ragnarok().cardID, size++);
+        put(new TalkToTheHand().cardID, size++);
+        put(new FlurryOfBlows().cardID, size++);
+        put(new Halt().cardID, size++);
+        put(new Prostrate().cardID, size++);
+        put(new SpiritShield().cardID, size++);
+        put(new SignatureMove().cardID, size++);
 
         // powers
-        add(new BattleHymn());
-        add(new Fasting());
-        add(new LikeWater());
-        add(new MentalFortress());
-        add(new Rushdown());
-        add(new Study());
-        add(new DevaForm());
-        add(new Devotion());
-        add(new Establishment());
-        add(new WhiteNoise());
+        put(new BattleHymn().cardID, size++);
+        put(new Fasting().cardID, size++);
+        put(new LikeWater().cardID, size++);
+        put(new MentalFortress().cardID, size++);
+        put(new Rushdown().cardID, size++);
+        put(new Study().cardID, size++);
+        put(new DevaForm().cardID, size++);
+        put(new Devotion().cardID, size++);
+        put(new Establishment().cardID, size++);
+        put(new WhiteNoise().cardID, size++);
 
-        add(new Alpha());
-        add(new WaveOfTheHand());
-        add(new BowlingBash());
-        add(new Brilliance());
-        add(new CarveReality());
-        add(new Collect());
-        add(new Conclude());
-        add(new ConjureBlade());
-        add(new Consecrate());
-        add(new CrushJoints());
-        add(new CutThroughFate());
-        add(new DeusExMachina());
-        add(new EmptyFist());
-        add(new EmptyMind());
-        add(new Eruption());
-        add(new Evaluate());
-        add(new FearNoEvil());
-        add(new FollowUp());
-        add(new ForeignInfluence());
-        add(new Foresight());
-        add(new Indignation());
-        add(new InnerPeace());
-        add(new Judgement());
-        add(new JustLucky());
-        add(new LessonLearned());
-        add(new Meditate());
-        add(new Nirvana());
-        add(new Pray());
-        add(new PressurePoints());
-        add(new ReachHeaven());
-        add(new SandsOfTime());
-        add(new SashWhip());
-        add(new Scrawl());
-        add(new SimmeringFury());
-        add(new Swivel());
-        add(new Tantrum());
-        add(new ThirdEye());
-        add(new Tranquility());
-        add(new Vault());
-        add(new Wallop());
-        add(new Weave());
-        add(new WheelKick());
-        add(new Wish());
-        add(new Blasphemy());
-
+        put(new Alpha().cardID, size++);
+        put(new WaveOfTheHand().cardID, size++);
+        put(new BowlingBash().cardID, size++);
+        put(new Brilliance().cardID, size++);
+        put(new CarveReality().cardID, size++);
+        put(new Collect().cardID, size++);
+        put(new Conclude().cardID, size++);
+        put(new ConjureBlade().cardID, size++);
+        put(new Consecrate().cardID, size++);
+        put(new CrushJoints().cardID, size++);
+        put(new CutThroughFate().cardID, size++);
+        put(new DeusExMachina().cardID, size++);
+        put(new EmptyFist().cardID, size++);
+        put(new EmptyMind().cardID, size++);
+        put(new Eruption().cardID, size++);
+        put(new Evaluate().cardID, size++);
+        put(new FearNoEvil().cardID, size++);
+        put(new FollowUp().cardID, size++);
+        put(new ForeignInfluence().cardID, size++);
+        put(new Foresight().cardID, size++);
+        put(new Indignation().cardID, size++);
+        put(new InnerPeace().cardID, size++);
+        put(new Judgement().cardID, size++);
+        put(new JustLucky().cardID, size++);
+        put(new LessonLearned().cardID, size++);
+        put(new Meditate().cardID, size++);
+        put(new Nirvana().cardID, size++);
+        put(new Pray().cardID, size++);
+        put(new PressurePoints().cardID, size++);
+        put(new ReachHeaven().cardID, size++);
+        put(new SandsOfTime().cardID, size++);
+        put(new SashWhip().cardID, size++);
+        put(new Scrawl().cardID, size++);
+        put(new SimmeringFury().cardID, size++);
+        put(new Swivel().cardID, size++);
+        put(new Tantrum().cardID, size++);
+        put(new ThirdEye().cardID, size++);
+        put(new Tranquility().cardID, size++);
+        put(new Vault().cardID, size++);
+        put(new Wallop().cardID, size++);
+        put(new Weave().cardID, size++);
+        put(new WheelKick().cardID, size++);
+        put(new Wish().cardID, size++);
+        put(new Blasphemy().cardID, size++);
 
         // blocks
-        add(new DeceiveReality());
-        add(new Vigilance());
-        add(new EmptyBody());
-        add(new Sanctity());
+        put(new DeceiveReality().cardID, size++);
+        put(new Vigilance().cardID, size++);
+        put(new EmptyBody().cardID, size++);
+        put(new Sanctity().cardID, size++);
 
 
         // retain cards towards the end?
-        add(new Crescendo());
-        add(new FlyingSleeves());
-        add(new WindmillStrike());
+        put(new Crescendo().cardID, size++);
+        put(new FlyingSleeves().cardID, size++);
+        put(new WindmillStrike().cardID, size++);
 
 
-        add(new Strike_Purple());
-        add(new Defend_Watcher());
-        add(new Protect());
-        add(new Perseverance());
-
-        return uglyThing;
-    }
-
-    private static void add(AbstractCard card) {
-        uglyThing.put(card.cardID, uglyThing.size());
-    }
+        put(new Strike_Purple().cardID, size++);
+        put(new Defend_Watcher().cardID, size++);
+        put(new Protect().cardID, size++);
+        put(new Perseverance().cardID, size++);
+    }};
 }
--- src/main/java/battleaimod/battleai/playorder/SilentPlayOrder.java
+++ src/main/java/battleaimod/battleai/playorder/SilentPlayOrder.java
@@ -1,6 +1,5 @@
 package battleaimod.battleai.playorder;
 
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.blue.Claw;
 import com.megacrit.cardcrawl.cards.blue.CreativeAI;
 import com.megacrit.cardcrawl.cards.colorless.*;
@@ -12,119 +11,108 @@ import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import java.util.HashMap;
 
 public class SilentPlayOrder {
-    public static HashMap<String, Integer> uglyThing;
-    public static final HashMap<String, Integer> CARD_RANKS = makeRank();
+    public static final HashMap<String, Integer> CARD_RANKS = new HashMap<String, Integer>() {{
+        int size = 0;
 
-    public static HashMap<String, Integer> makeRank() {
-        uglyThing = new HashMap<>();
+        put(new Panache().cardID, size++);
+        put(new GrandFinale().cardID, size++);
+        put(new SadisticNature().cardID, size++);
+        put(new Apotheosis().cardID, size++);
+        put(new CreativeAI().cardID, size++);
+        put(new LessonLearned().cardID, size++);
+        put(new WaveOfTheHand().cardID, size++);
 
+        put(new Backflip().cardID, size++);
+        put(new Acrobatics().cardID, size++);
+        put(new BulletTime().cardID, size++);
+        put(new Transmutation().cardID, size++);
+        put(new Claw().cardID, size++);
 
-        add(new Panache());
-        add(new GrandFinale());
-        add(new SadisticNature());
-        add(new Apotheosis());
-        add(new CreativeAI());
-        add(new LessonLearned());
-        add(new WaveOfTheHand());
+        put(new Flechettes().cardID, size++);
+        put(new Burst().cardID, size++);
+        put(new Apparition().cardID, size++);
+        put(new Footwork().cardID, size++);
+        put(new Adrenaline().cardID, size++);
+        put(new Malaise().cardID, size++);
+        put(new ToolsOfTheTrade().cardID, size++);
+        put(new Choke().cardID, size++);
+        put(new AfterImage().cardID, size++);
+        put(new Caltrops().cardID, size++);
+        put(new InfiniteBlades().cardID, size++);
+        put(new LegSweep().cardID, size++);
+        put(new NoxiousFumes().cardID, size++);
+        put(new CripplingPoison().cardID, size++);
+        put(new Envenom().cardID, size++);
+        put(new AThousandCuts().cardID, size++);
+        put(new Nightmare().cardID, size++);
+        put(new Alchemize().cardID, size++);
+        put(new PhantasmalKiller().cardID, size++);
+        put(new Outmaneuver().cardID, size++);
+        put(new Accuracy().cardID, size++);
+        put(new CalculatedGamble().cardID, size++);
 
-        add(new Backflip());
-        add(new Acrobatics());
-        add(new BulletTime());
-        add(new Transmutation());
-        add(new Claw());
+        put(new BouncingFlask().cardID, size++);
+        put(new CorpseExplosion().cardID, size++);
+        put(new DeadlyPoison().cardID, size++);
+        put(new PoisonedStab().cardID, size++);
 
-        add(new Flechettes());
-        add(new Burst());
-        add(new Apparition());
-        add(new Footwork());
-        add(new Adrenaline());
-        add(new Malaise());
-        add(new ToolsOfTheTrade());
-        add(new Choke());
-        add(new AfterImage());
-        add(new Caltrops());
-        add(new InfiniteBlades());
-        add(new LegSweep());
-        add(new NoxiousFumes());
-        add(new CripplingPoison());
-        add(new Envenom());
-        add(new AThousandCuts());
-        add(new Nightmare());
-        add(new Alchemize());
-        add(new PhantasmalKiller());
-        add(new Outmaneuver());
-        add(new Accuracy());
-        add(new CalculatedGamble());
+        put(new Catalyst().cardID, size++);
 
-        add(new BouncingFlask());
-        add(new CorpseExplosion());
-        add(new DeadlyPoison());
-        add(new PoisonedStab());
+        put(new Terror().cardID, size++);
+        put(new WellLaidPlans().cardID, size++);
 
-        add(new Catalyst());
+        put(new Dash().cardID, size++);
+        put(new Skewer().cardID, size++);
+        put(new RiddleWithHoles().cardID, size++);
+        put(new Neutralize().cardID, size++);
+        put(new QuickSlash().cardID, size++);
+        put(new SuckerPunch().cardID, size++);
+        put(new AllOutAttack().cardID, size++);
+        put(new Backstab().cardID, size++);
+        put(new HeelHook().cardID, size++);
+        put(new Bane().cardID, size++);
+        put(new FlyingKnee().cardID, size++);
+        put(new DaggerThrow().cardID, size++);
+        put(new DieDieDie().cardID, size++);
+        put(new Slice().cardID, size++);
+        put(new DaggerSpray().cardID, size++);
+        put(new GlassKnife().cardID, size++);
+        put(new MasterfulStab().cardID, size++);
+        put(new Eviscerate().cardID, size++);
+        put(new Predator().cardID, size++);
+        put(new CloakAndDagger().cardID, size++);
 
-        add(new Terror());
-        add(new WellLaidPlans());
+        put(new SneakyStrike().cardID, size++);
 
+        put(new Shiv().cardID, size++);
 
-        add(new Dash());
-        add(new Skewer());
-        add(new RiddleWithHoles());
-        add(new Neutralize());
-        add(new QuickSlash());
-        add(new SuckerPunch());
-        add(new AllOutAttack());
-        add(new Backstab());
-        add(new HeelHook());
-        add(new Bane());
-        add(new FlyingKnee());
-        add(new DaggerThrow());
-        add(new DieDieDie());
-        add(new Slice());
-        add(new DaggerSpray());
-        add(new GlassKnife());
-        add(new MasterfulStab());
-        add(new Eviscerate());
-        add(new Predator());
-        add(new CloakAndDagger());
+        put(new StormOfSteel().cardID, size++);
+        put(new BladeDance().cardID, size++);
+        put(new Strike_Green().cardID, size++);
 
-        add(new SneakyStrike());
+        put(new Reflex().cardID, size++);
+        put(new Setup().cardID, size++);
+        put(new Tactician().cardID, size++);
+        put(new WraithForm().cardID, size++);
 
-        add(new Shiv());
+        put(new Unload().cardID, size++);
 
-        add(new StormOfSteel());
-        add(new BladeDance());
-        add(new Strike_Green());
+        put(new Finisher().cardID, size++);
 
-        add(new Reflex());
-        add(new Setup());
-        add(new Tactician());
-        add(new WraithForm());
+        put(new Doppelganger().cardID, size++);
 
-        add(new Unload());
+        put(new Survivor().cardID, size++);
+        put(new Prepared().cardID, size++);
+        put(new PiercingWail().cardID, size++);
+        put(new Distraction().cardID, size++);
+        put(new EndlessAgony().cardID, size++);
+        put(new DodgeAndRoll().cardID, size++);
+        put(new Blur().cardID, size++);
+        put(new EscapePlan().cardID, size++);
+        put(new Deflect().cardID, size++);
+        put(new Defend_Green().cardID, size++);
 
-        add(new Finisher());
-
-        add(new Doppelganger());
-
-        add(new Survivor());
-        add(new Prepared());
-        add(new PiercingWail());
-        add(new Distraction());
-        add(new EndlessAgony());
-        add(new DodgeAndRoll());
-        add(new Blur());
-        add(new EscapePlan());
-        add(new Deflect());
-        add(new Defend_Green());
-
-        add(new Expertise());
-        add(new Concentrate());
-
-        return uglyThing;
-    }
-
-    private static void add(AbstractCard card) {
-        uglyThing.put(card.cardID, uglyThing.size());
-    }
+        put(new Expertise().cardID, size++);
+        put(new Concentrate().cardID, size++);
+    }};
 }
