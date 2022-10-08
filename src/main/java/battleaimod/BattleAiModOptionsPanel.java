a920c9c0d387fc2dbec78a51ce60574d154c94d2
boardengineer <boardengineer88@gmail.com>
2022-10-07 21:21:24 -0700

    Rename controller mode dropdown

--- src/main/java/battleaimod/BattleAiModOptionsPanel.java
+++ src/main/java/battleaimod/BattleAiModOptionsPanel.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 public class BattleAiModOptionsPanel extends ModPanel implements DropdownMenuListener {
     private static final float LABEL_X_POS = Settings.WIDTH / 5.0F;
     private static final float LABEL_Y_POS = Settings.HEIGHT * 2.0F / 3.0F;
-    public static DropdownMenu characters;
+    public static DropdownMenu controllerMode;
 
     public BattleAiModOptionsPanel() {
         ModLabel controllerModeLabel = new ModLabel(
@@ -27,26 +27,26 @@ public class BattleAiModOptionsPanel extends ModPanel implements DropdownMenuLis
         String[] values = Arrays.stream(BattleClientController.ControllerMode.values())
                                 .map(BattleClientController.ControllerMode::toString)
                                 .toArray(String[]::new);
-        characters = new DropdownMenu(this, values, FontHelper.tipBodyFont, Settings.CREAM_COLOR);
-        characters.setSelectedIndex(BattleAiMod.battleClientControllerMode.ordinal());
+        controllerMode = new DropdownMenu(this, values, FontHelper.tipBodyFont, Settings.CREAM_COLOR);
+        controllerMode.setSelectedIndex(BattleAiMod.battleClientControllerMode.ordinal());
     }
 
     @Override
     public void update() {
         super.update();
-        characters.update();
+        controllerMode.update();
     }
 
     @Override
     public void render(SpriteBatch sb) {
         super.render(sb);
 
-        characters.render(sb, LABEL_X_POS + 350 * Settings.scale, LABEL_Y_POS + 22 * Settings.scale);
+        controllerMode.render(sb, LABEL_X_POS + 350 * Settings.scale, LABEL_Y_POS + 22 * Settings.scale);
     }
 
     @Override
     public void changedSelectionTo(DropdownMenu dropdownMenu, int i, String s) {
-        if (dropdownMenu == characters) {
+        if (dropdownMenu == controllerMode) {
             BattleAiMod.battleClientControllerMode = BattleClientController.ControllerMode
                     .valueOf(s);
             BattleClientController.saveMode(BattleAiMod.battleClientControllerMode);
